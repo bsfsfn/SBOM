ee1c97de8343303e2a852d784504962d33e03d1a
bsfsfn <20731289+bsfsfn@users.noreply.github.com>
2024-06-10 12:25:18 +0200

    Add `package-lock.json` support

--- sbom.py
+++ sbom.py
@@ -28,29 +28,51 @@ def parse_pip(path):
 
    return deps
 
-# assumes keys point to a version, not URL or path
-# version ranges are reported unmodified
+# package.json specification docs: https://docs.npmjs.com/cli/configuring-npm/package-json
+# assumes all the dependencies are contained within these keys:
+DEPS_KEYS = ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']
+
+# versions are reported unmodified (they can also be ranges, URLs, paths)
 def parse_npm(path):
    deps = {}
 
    with open(path) as file:
       data = json.load(file)
 
-      # package.json specification docs: https://docs.npmjs.com/cli/configuring-npm/package-json
-      # assumes all the dependencies are contained within these keys:
-      deps_keys = ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']
-
-      for dep_key in deps_keys:
+      for dep_key in DEPS_KEYS:
          deps_chunk = data.get(dep_key)
+
          if deps_chunk is not None:
             deps |= deps_chunk
 
    return deps
 
+# package-lock.json specification docs: https://docs.npmjs.com/cli/configuring-npm/package-lock-json
+# assumes lockfileVersion >= 2 (doesn't parse the `dependencies` key, only `packages`)
+# versions are reported unmodified (they can also be ranges, URLs, paths)
+# package names can be local paths, these are reported unmodified
 def parse_npmlock(path):
-   deps = {}
+   deps = []
 
-   # TODO
+   with open(path) as file:
+      data = json.load(file)
+
+      packages_data = data.get('packages')
+
+      for pkg_name, pkg_data in packages_data.items():
+         if pkg_name == '':
+            # this data was already parsed in `parse_npm`
+            continue
+
+         pkg_ver = pkg_data.get('version')
+         deps.append((pkg_name, pkg_ver))
+
+         for dep_key in DEPS_KEYS:
+            deps_chunk = pkg_data.get(dep_key)
+
+            if deps_chunk is not None:
+               for dep_name, dep_ver in deps_chunk.items():
+                  deps.append((dep_name, dep_ver))
 
    return deps
 
@@ -109,7 +131,17 @@ for repo_path in git_repo_paths:
 
       npmlock_path = os.path.join(repo_path, 'package-lock.json')
       npmlock_deps = parse_npmlock(npmlock_path)
-      # TODO
+
+      for dep in npmlock_deps:
+         dep_d = {}
+
+         dep_d['name'] = dep[0]
+         dep_d['version'] = dep[1]
+         dep_d['type'] = 'npm'
+         dep_d['path'] = npmlock_path
+         dep_d['commit_hash'] = commit_hash
+
+         sbom_data.append(dep_d)
 
 # sort SBOM by name then version
 sbom_data = sorted(sbom_data, key = lambda x: (x['name'], x['version']))
