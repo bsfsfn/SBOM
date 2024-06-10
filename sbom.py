b2acf8a8cb8893ddbc970b7d04bd6ad62781120c
bsfsfn <20731289+bsfsfn@users.noreply.github.com>
2024-06-10 13:15:43 +0200

    SBOM creation code quality improvements

--- sbom.py
+++ sbom.py
@@ -103,6 +103,22 @@ def get_commit_hash(repo_path):
 
    return sha
 
+def create_sbom_entry(name, version, type, path, commit_hash):
+   """Creates a SBOM entry with the given values.
+   
+   The SBOM is a list of SBOM entry dictionaries. Creating SBOM entries using exclusively this method ensures a consistent structure.
+   
+   Returns the created SBOM entry dictionary."""
+   dep_d = {}
+
+   dep_d['name'] = name
+   dep_d['version'] = version
+   dep_d['type'] = type
+   dep_d['path'] = path
+   dep_d['commit_hash'] = commit_hash
+
+   return dep_d
+
 if len(sys.argv) != 2:
    print('Error: incorrect number of arguments.')
    print(f'Usage: {sys.argv[0]} <directory>')
@@ -113,7 +129,7 @@ git_repo_paths = find_git_repo_paths(directory_path)
 
 print(f'Found {len(git_repo_paths)} git repositories in \'{directory_path}\'')
 
-sbom_data = []
+sbom = []
 
 # find all dependencies
 for repo_path in git_repo_paths:
@@ -126,29 +142,17 @@ for repo_path in git_repo_paths:
       pip_deps = parse_pip(pip_path)
 
       for dep in pip_deps:
-         dep_d = {}
+         sbom_entry = create_sbom_entry(dep, pip_deps[dep], 'pip', pip_path, commit_hash)
 
-         dep_d['name'] = dep
-         dep_d['version'] = pip_deps[dep]
-         dep_d['type'] = 'pip'
-         dep_d['path'] = pip_path
-         dep_d['commit_hash'] = commit_hash
-
-         sbom_data.append(dep_d)
+         sbom.append(sbom_entry)
 
    if os.path.exists(npm_path):
       npm_deps = parse_npm(npm_path)
 
       for dep in npm_deps:
-         dep_d = {}
+         sbom_entry = create_sbom_entry(dep[0], dep[1], 'npm', npm_path, commit_hash)
 
-         dep_d['name'] = dep[0]
-         dep_d['version'] = dep[1]
-         dep_d['type'] = 'npm'
-         dep_d['path'] = npm_path
-         dep_d['commit_hash'] = commit_hash
-
-         sbom_data.append(dep_d)
+         sbom.append(sbom_entry)
 
       npmlock_path = os.path.join(repo_path, 'package-lock.json')
 
@@ -156,31 +160,25 @@ for repo_path in git_repo_paths:
          npmlock_deps = parse_npmlock(npmlock_path)
 
          for dep in npmlock_deps:
-            dep_d = {}
+            sbom_entry = create_sbom_entry(dep[0], dep[1], 'npm', npmlock_path, commit_hash)
 
-            dep_d['name'] = dep[0]
-            dep_d['version'] = dep[1]
-            dep_d['type'] = 'npm'
-            dep_d['path'] = npmlock_path
-            dep_d['commit_hash'] = commit_hash
-
-            sbom_data.append(dep_d)
+            sbom.append(sbom_entry)
 
 # sort the SBOM by name then version
-sbom_data = sorted(sbom_data, key = lambda x: (x['name'], x['version']))
+sbom = sorted(sbom, key = lambda x: (x['name'], x['version']))
 
 # save the SBOM to .csv and .json files
 csv_path = os.path.join(directory_path, 'sbom.csv')
 json_path = os.path.join(directory_path, 'sbom.json')
 
 with open(csv_path, 'w', newline = '') as file:
-   wr = csv.DictWriter(file, sbom_data[0].keys())
+   wr = csv.DictWriter(file, sbom[0].keys())
    wr.writeheader()
-   wr.writerows(sbom_data)
+   wr.writerows(sbom)
 
    print('Saved SBOM in CSV format to', csv_path)
 
 with open(json_path, 'w') as file:
-   json.dump(sbom_data, file, indent = 3)
+   json.dump(sbom, file, indent = 3)
 
    print('Saved SBOM in CSV format to', json_path)
